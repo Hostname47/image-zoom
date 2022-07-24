256a43d2dca154ac66809762a893c197bd8ef128
HOSTNAME47 <mouadstev1@gmail.com>
2022-07-25 00:04:11 +0100

    implement the zoom

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-let zoomConfig = {}
+let mediaPreview = document.querySelector('#media-preview');
+let mediaPreviewImage = mediaPreview.querySelector('.image');
 let zoomBox = document.querySelector('#media-zoom-box');
+let zoomImage = document.querySelector('#media-zoom-image');
 let lens = document.querySelector('#zoom-lens');
+let config = {
+    halfLensWidth: 0,
+    halfLensHeight: 0,
+    zoomedImage: {
+        width: 0,
+        height: 0,
+    },
+    lens: {
+        left: 0,
+        top: 0
+    }
+}
 
 document.querySelector('#media-preview').addEventListener('mouseenter', (event) => {
     zoomBox.classList.remove('none');
     lens.classList.remove('none');
+    // Lens dimensions will be able to be updated once we'll add lens precision increment/decrement
+    config.halfLensWidth = lens.clientWidth / 2;
+    config.halfLensHeight = lens.clientHeight / 2;
+
+    /**
+     * To handle zoom section, we need to perform two key steps:
+     *  1. Zoom the image first by increasing background size of image
+     *  2. Find the background position to only display the hovered section of the image
+     */
+    let zoomImageWidth = zoomBox.clientWidth / lens.clientWidth * zoomBox.clientWidth;
+    let zoomImageHeight = zoomBox.clientHeight / lens.clientHeight * zoomBox.clientHeight;
+
+    zoomImage.style.backgroundSize = `${zoomImageWidth}px ${zoomImageHeight}px`;
+
+    config.zoomedImage.width = zoomImageWidth;
+    config.zoomedImage.height = zoomImageHeight;
 });
 
 document.querySelector('#media-preview').addEventListener('mousemove', (event) => {
@@ -12,7 +42,8 @@ document.querySelector('#media-preview').addEventListener('mousemove', (event) =
     let x = event.clientX - bounds.left;
     let y = event.clientY - bounds.top;
 
-    moveLens(lens, x, y);
+    moveLens(x, y);
+    moveImage();
 });
 
 document.querySelector('#media-preview').addEventListener('mouseleave', (event) => {
@@ -20,12 +51,35 @@ document.querySelector('#media-preview').addEventListener('mouseleave', (event) 
     lens.classList.add('none');
 });
 
-function moveLens(lens, x, y) {
+function moveLens(x, y) {
     // Handling lens bounderies to keep it inside the media box
-    if(x <= 70) x = 70;
-    if(y <= 70) y = 70;
-    if(x >= zoomBox.clientWidth - 70) x = zoomBox.clientWidth - 70;
-    if(y >= zoomBox.clientHeight - 70) y = zoomBox.clientHeight - 70;
+    if(x <= config.halfLensWidth) x = config.halfLensWidth;
+    if(y <= config.halfLensHeight) y = config.halfLensHeight;
+    if(x >= mediaPreview.clientWidth - config.halfLensWidth) x = mediaPreview.clientWidth - config.halfLensWidth;
+    if(y >= mediaPreview.clientHeight - config.halfLensHeight) y = mediaPreview.clientHeight - config.halfLensHeight;
     
-    lens.style.cssText = `left: ${x - 70}px; top: ${y - 70}px;`;
+    let lensLeft = x - config.halfLensWidth;
+    let lensTop = y - config.halfLensHeight;
+
+    lens.style.cssText = `left: ${lensLeft}px; top: ${lensTop}px;`;
+
+    // Set lens details to config
+    config.lens.left = lensLeft;
+    config.lens.top = lensTop;
+}
+
+/**
+ * This function basically move the background zoom image to only show the portion covered
+ * by the lens. It takes the coordinates of the lens and calculate that coordinates relatively 
+ * in the zoomed image and use those values to set the background position.
+ */
+function moveImage() {
+    let left = config.lens.left;
+    let top = config.lens.top;
+
+    let coordinates = {
+        x: config.zoomedImage.width * left / mediaPreview.clientWidth,
+        y: config.zoomedImage.height * top / mediaPreview.clientHeight,
+    }
+    zoomImage.style.backgroundPosition = `${-coordinates.x}px ${-coordinates.y}px`;
 }
